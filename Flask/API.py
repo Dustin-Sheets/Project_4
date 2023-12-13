656e78c2d0c14e3ec954125a77779c93cedcc51c
vincent@t-101.com <vincent@t-101.com>
2023-12-13 11:19:46 -0600

    comments

--- Flask/API.py
+++ Flask/API.py
@@ -1,96 +1,62 @@
 import os
-import joblib
 from flask import Flask, render_template, request, jsonify
-#from tensorflow import keras
-import tensorflow as tf
 import xgboost as xgb
-import numpy as np
-
-
-
-
-# Load Scaled and  Trained model
-#current_dir = os.path.dirname(os.path.realpath(__file__))
-#scaler_path = os.path.join(current_dir, '..','Machine_learning_prediction','Models','scaler_model.save')
-#model_path = os.path.join(current_dir, '..','Machine_learning_prediction','Models','Heart_Disease_Pred.h5')
-
-#scaler = joblib.load(scaler_path)
-#model = tf.keras.models.load_model(model_path)
-
-
-
-#def predict_heart_disease(model, new_data):
-#    prediction = model.predict(new_data)
-#    return prediction.tolist()
-
 
 
 
 app = Flask(__name__)
 
-############ now to use xgboost model
-
+# Load the model
 current_dir = os.path.dirname(os.path.realpath(__file__))
 model_path = os.path.join(current_dir, '..','Machine_learning_prediction','Models','xgboost_best_model.model')
-
 xgboost_model = xgb.Booster()
 xgboost_model.load_model(model_path)
 
 
 
-
-
-
-
+# Route to the home page
 @app.route('/')
 def home():
     return render_template('dashboard.html')
 
 
 
-
+# Route executed when the user clicks the "Predict" button
 @app.route('/predict', methods=['POST'])
 def predict():
+    
     try:
         # Extract JSON data from POST request (Array data)
         data = request.get_json()
+
 
         # Ensure data is in the correct format as a 2-D array
         if not isinstance(data, list) or not all(isinstance(row, list) for row in data):
             raise ValueError("Input data must be a list of lists.")
         
+        
         # Convert data to DMatrix object (XGBoost format)
         data_dmatrix = xgb.DMatrix(data)
 
-        # Check the shape of the data
-        #if data.shape[1] != 16: 
-        #    raise ValueError("Each data sample should contain 16 features.")
-        
-        
-        # Scale input data
-        #new_data_scaled = scaler.transform(data)
 
         # Make a prediction
-        #data_prediction = predict_heart_disease(model, new_data_scaled)
         data_prediction = xgboost_model.predict(data_dmatrix)
 
 
-
         # Convert the prediction to a useful format
-        #response = {
-       #     'prediction': data_prediction.tolist()
-        #}
         response = {'prediction': data_prediction.tolist()}
 
 
         
-        # Return the response as JSON
-        #return jsonify({'prediction': data_prediction}), 200
+        # Return the response as JSON with error handling
         return jsonify(response), 200
     except ValueError as ve:
         return jsonify({'error': 'ValueError: ' + str(ve)}), 400
     except Exception as e:
         return jsonify({'error': 'Exception: ' + str(e)}), 500
-
+    
+    
+# to run the app
 if __name__ == '__main__':
     app.run(debug=False)  # Set debug to False for production
+    
--- Flask/static/dustinsjs.js
+++ Flask/static/dustinsjs.js
@@ -1,150 +1,149 @@
-TF = {"TrueFalse": ["Yes", "No"]}
+TF = {"TrueFalse": ["Yes", "No"]} 
 MF = {"MaleFemale": ["Male","Female"]}
 BMI = {"BMI":['18','20','22','24','26','28',
 '30','32','34','36','38','40','42','44']}
 
-    // High Blood Pressure?
-    for (i=0;i<2; i++){
-        TF.TrueFalse[i]
 
-        d3.select("#selDataset1").append("option").text(TF.TrueFalse[i]).
-        attr("value", TF.TrueFalse[i]);
-    }
+// High Blood Pressure?
+for (i=0;i<2; i++){
+    TF.TrueFalse[i]
 
-    // High Cholesterol?
-    for (i=0;i<2; i++){
-        TF.TrueFalse[i]
+    d3.select("#selDataset1").append("option").text(TF.TrueFalse[i]).
+    attr("value", TF.TrueFalse[i]);
+};
 
-        d3.select("#selDataset2").append("option").text(TF.TrueFalse[i]).
-        attr("value", TF.TrueFalse[i]);
-    }
+// High Cholesterol?
+for (i=0;i<2; i++){
+    TF.TrueFalse[i]
 
-    // Colesterol checked?
-    for (i=0;i<2; i++){
-        TF.TrueFalse[i]
+    d3.select("#selDataset2").append("option").text(TF.TrueFalse[i]).
+    attr("value", TF.TrueFalse[i]);
+};
 
-        d3.select("#selDataset3").append("option").text(TF.TrueFalse[i]).
-        attr("value", TF.TrueFalse[i]);
-    }
+// Colesterol checked?
+for (i=0;i<2; i++){
+    TF.TrueFalse[i]
 
-    // BMI?
-    for (i=0;i<BMI.BMI.length; i++){
-        BMI.BMI[i]
+    d3.select("#selDataset3").append("option").text(TF.TrueFalse[i]).
+    attr("value", TF.TrueFalse[i]);
+};
 
-        d3.select("#selDataset4").append("option").text(BMI.BMI[i]).
-        attr("value", BMI.BMI[i]);
-    }
+// BMI?
+for (i=0;i<BMI.BMI.length; i++){
+    BMI.BMI[i]
 
-    // Smoke?
-    for (i=0;i<2; i++){
-        TF.TrueFalse[i]
+    d3.select("#selDataset4").append("option").text(BMI.BMI[i]).
+    attr("value", BMI.BMI[i]);
+};
 
-        d3.select("#selDataset5").append("option").text(TF.TrueFalse[i]).
-        attr("value", TF.TrueFalse[i]);
-    }
+// Smoke?
+for (i=0;i<2; i++){
+    TF.TrueFalse[i]
 
-    // Stroke?
-    for (i=0;i<2; i++){
-        TF.TrueFalse[i]
+    d3.select("#selDataset5").append("option").text(TF.TrueFalse[i]).
+    attr("value", TF.TrueFalse[i]);
+};
 
-        d3.select("#selDataset6").append("option").text(TF.TrueFalse[i]).
-        attr("value", TF.TrueFalse[i]);
-    }
+// Stroke?
+for (i=0;i<2; i++){
+    TF.TrueFalse[i]
 
-    // Diabetes?
-    const diabetesCategories = ["No or during pregnancy", "Pre-diabetes or borderline diabetes", "Yes diabetes"];
-    for (let i = 0; i < diabetesCategories.length; i++) {
-        d3.select("#selDataset7").append("option").text(diabetesCategories[i]).attr("value", i);
-    }
+    d3.select("#selDataset6").append("option").text(TF.TrueFalse[i]).
+    attr("value", TF.TrueFalse[i]);
+};
 
-    // physically active?
-    for (i=0;i<2; i++){
-        TF.TrueFalse[i]
+// Diabetes?
+const diabetesCategories = ["No or during pregnancy", "Pre-diabetes or borderline diabetes", "Yes diabetes"];
+for (let i = 0; i < diabetesCategories.length; i++) {
+    d3.select("#selDataset7").append("option").text(diabetesCategories[i]).attr("value", i);
+};
 
-        d3.select("#selDataset8").append("option").text(TF.TrueFalse[i]).
-        attr("value", TF.TrueFalse[i]);
-    }
+// physically active?
+for (i=0;i<2; i++){
+    TF.TrueFalse[i]
 
-    // Eat Fruits?
-    for (i=0;i<2; i++){
-        TF.TrueFalse[i]
+    d3.select("#selDataset8").append("option").text(TF.TrueFalse[i]).
+    attr("value", TF.TrueFalse[i]);
+};
 
-        d3.select("#selDataset9").append("option").text(TF.TrueFalse[i]).
-        attr("value", TF.TrueFalse[i]);
-    }
+// Eat Fruits?
+for (i=0;i<2; i++){
+    TF.TrueFalse[i]
 
-    // Eat Vegetables?
-    for (i=0;i<2; i++){
-        TF.TrueFalse[i]
+    d3.select("#selDataset9").append("option").text(TF.TrueFalse[i]).
+    attr("value", TF.TrueFalse[i]);
+};
 
-        d3.select("#selDataset10").append("option").text(TF.TrueFalse[i]).
-        attr("value", TF.TrueFalse[i]);
-    }
+// Eat Vegetables?
+for (i=0;i<2; i++){
+    TF.TrueFalse[i]
 
-    // Drink Alcohol?
-    for (i=0;i<2; i++){
-        TF.TrueFalse[i]
+    d3.select("#selDataset10").append("option").text(TF.TrueFalse[i]).
+    attr("value", TF.TrueFalse[i]);
+};
 
-        d3.select("#selDataset11").append("option").text(TF.TrueFalse[i]).
-        attr("value", TF.TrueFalse[i]);
-    }
+// Drink Alcohol?
+for (i=0;i<2; i++){
+    TF.TrueFalse[i]
 
-    // Doctor checkup regularly?
-    for (i=0;i<2; i++){
-        TF.TrueFalse[i]
+    d3.select("#selDataset11").append("option").text(TF.TrueFalse[i]).
+    attr("value", TF.TrueFalse[i]);
+};
 
-        d3.select("#selDataset12").append("option").text(TF.TrueFalse[i]).
-        attr("value", TF.TrueFalse[i]);
-    }
+// Doctor checkup regularly?
+for (i=0;i<2; i++){
+    TF.TrueFalse[i]
 
-
-    // Walking difficulty?
-    for (i=0;i<2; i++){
-        TF.TrueFalse[i]
-
-        d3.select("#selDataset14").append("option").text(TF.TrueFalse[i]).
-        attr("value", TF.TrueFalse[i]);
-    }
-
-    // Sex?
-    for (i=0;i<2; i++){
-        MF.MaleFemale[i]
-
-        d3.select("#selDataset15").append("option").text(MF.MaleFemale[i]).
-        attr("value", MF.MaleFemale[i]);
-    }
-
-    // Age?
-
-    for (i=0;i<120; i++){
-        d3.select("#selDataset16").append("option").text(i).
-        attr("value", i);
-    }
+    d3.select("#selDataset12").append("option").text(TF.TrueFalse[i]).
+    attr("value", TF.TrueFalse[i]);
+};
 
 
-var elements = document.querySelectorAll("[id^=selDataset]");
+// Walking difficulty?
+for (i=0;i<2; i++){
+    TF.TrueFalse[i]
 
+    d3.select("#selDataset14").append("option").text(TF.TrueFalse[i]).
+    attr("value", TF.TrueFalse[i]);
+};
 
+// Sex?
+for (i=0;i<2; i++){
+    MF.MaleFemale[i]
 
-// ..
+    d3.select("#selDataset15").append("option").text(MF.MaleFemale[i]).
+    attr("value", MF.MaleFemale[i]);
+};
+
+// Age?
+
+for (i=0;i<120; i++){
+    d3.select("#selDataset16").append("option").text(i).
+    attr("value", i);
+};
 
 
 
 
 
+
+// Function to submit data to the Flask API
 function submitData() {
     
 
-    // Define a mapping for the Diabetes variable
+    // Define a mapping for the Diabetes variable. Defined values by default in dataset
     const diabetesMap = {
         "No or during pregnancy": 0,
         "Pre-diabetes or borderline diabetes": 1,
         "Yes diabetes": 2
     };
 
+    // Get all values the form the user filled in
     const elements = document.querySelectorAll("[id^=selDataset]");
 
-    // Get the values from the form elements
+
+    // Create an array of the values to send to the Flask API by using for loop to iterate 
+    // through the elements array and push the value of each element to the elementsArr array 
     var elementsArr = [];
     for (let i = 0; i < elements.length; i++) {
         const element = elements[i];
@@ -152,7 +151,7 @@ function submitData() {
         
 
 
-
+        // Convert the diabetes value to a number
         if (element.id === "selDataset7") {
             value = +value; // Map the value using the diabetesMap
         
@@ -167,50 +166,61 @@ function submitData() {
         }
         
        
-
+        // Push the value to the elementsArr array
         elementsArr.push(value);
 
 
 
         // Log the JSON payload being sent to the Flask API
         console.log('Request Payload:', JSON.stringify([elementsArr]));
-    }
+    };
 
     
+
+
+
+
     // Send data to Flask API for prediction
     fetch('/predict', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify([elementsArr]), // Ensure data is sent as a 2D array
+        body: JSON.stringify([elementsArr]), // Ensure data is sent as a 2D array by wrapping in [] and jsonifying
     })
     .then(response => {
         if (!response.ok) {
-            throw new Error('Network response was not ok: ' + response.statusText);
+            throw new Error('Network response was not ok: ' + response.statusText); // Throw an error if the response is not ok
         }
-        return response.json();
+        return response.json();// Return the response as JSON when ok
     })
-    .then(data => {
-        console.log('Success:', data.prediction);  // Log the prediction data
-        const predictionBox = document.getElementById("predictionBox");
-        const predictionValue = data.prediction[0];
-        const prediction_percent = (predictionValue * 100).toFixed(2);
+    .then(data => { 
+        console.log('Success:', data.prediction);                           // Log the prediction data to the console
+        const predictionBox = document.getElementById("predictionBox");     // Get the prediction box element
+        const predictionValue = data.prediction[0];                         // Get the prediction value from the response data
+        const prediction_percent = (predictionValue * 100).toFixed(2);      // Convert the prediction value to a percentage
 
-        if (prediction_percent > 40) {
+        if (prediction_percent > 40) {                                      // If the prediction percentage is greater than 40%, the patient is at risk
             predictionBox.innerHTML = "Our model predicts that you are at risk of having heart attack/failure with a " + prediction_percent + "% " + " Please consult your doctor.";
         } else {
             predictionBox.innerHTML = "Our model predicts that you are not at risk of having heart attack/failure with a " + prediction_percent + "% " + " . No immediate concerns, but consider regular checkups.";
         }
     })
     .catch((error) => {
-        console.error('Error:', error);
+        console.error('Error:', error);// Log any errors to the console if there are any
     });
-}
+};
 
-// Attach the `submitData` function to the click event of the submit button
+
+
+
+
+
+
+// Add an event listener to the submit button to call the submitData function when clicked 
+
 document.getElementById("submitBtn").addEventListener("click", function(event) {
-    event.preventDefault(); // Prevent the default form submission
-    submitData();
+    event.preventDefault();                                                     // Prevent the page from refreshing when the button is clicked
+    submitData();                                                               // Call the submitData function
 });
 
